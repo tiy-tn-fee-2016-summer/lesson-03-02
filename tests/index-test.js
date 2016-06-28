117994d14017b2c2fd4e7f8d108dedde1a78f1e8
Ryan Tablada <ryan.tablada@gmail.com>
2016-06-28 11:59:35 -0500

    Create isEven function

--- tests/index-test.js
+++ tests/index-test.js
@@ -2,10 +2,12 @@
 
 /* global require */
 
+// Anonymous function set to a variable
 const lessThanFive = function (number) {
   return number < 5;
 };
 
+// Arrow Function
 const testLessThanFive = (check) => {
   check.equal(lessThanFive(0), true, 'Zero is always less than five');
   check.equal(lessThanFive(4), true, 'Four is always less than five');
@@ -13,8 +15,19 @@ const testLessThanFive = (check) => {
   check.equal(lessThanFive(5), false, 'Five is never less than itself');
 };
 
+// Function expression
+function isEven(number) {
+  return number % 2 === 0;
+}
+
+const testIsEven = (check) => {
+  check.equal(isEven(1), false);
+  check.equal(isEven(612), true);
+  check.equal(isEven(0), true);
+};
+
 test('lessThanFive', testLessThanFive);
-// test('isEven');
+test('isEven', testIsEven);
 // test('isOdd');
 // test('digitize');
 // test('sumOfDigits');
