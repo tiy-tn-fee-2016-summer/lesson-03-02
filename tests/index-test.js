6d6bd8b381121b05c9d25c79abe4236307aa61d4
Ryan Tablada <ryan.tablada@gmail.com>
2016-06-28 11:12:07 -0500

    Implement lessThanFive

--- tests/index-test.js
+++ tests/index-test.js
@@ -1,3 +1,26 @@
 'use strict';
 
 /* global require */
+
+const lessThanFive = function (number) {
+  if (number < 5) {
+    return true;
+  }
+
+  return false;
+};
+
+const testLessThanFive = (check) => {
+  check.equal(lessThanFive(0), true, 'Zero is always less than five');
+  check.equal(lessThanFive(1), true, 'One is always less than five');
+  check.equal(lessThanFive(7), false, 'Seven is never less than five');
+  check.equal(lessThanFive(2), true, 'Two is always less than five');
+  check.equal(lessThanFive(5), false, 'Five is never less than itself');
+};
+
+test('lessThanFive', testLessThanFive);
+// test('isEven');
+// test('isOdd');
+// test('digitize');
+// test('sumOfDigits');
+// test('evenDigitsAsArray');
