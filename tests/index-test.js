09099f49e8a4645f10eba1f7e181e124f7c47983
Ryan Tablada <ryan.tablada@gmail.com>
2016-06-28 11:30:00 -0500

    Clean up test and unnecessary if statements

--- tests/index-test.js
+++ tests/index-test.js
@@ -3,18 +3,13 @@
 /* global require */
 
 const lessThanFive = function (number) {
-  if (number < 5) {
-    return true;
-  }
-
-  return false;
+  return number < 5;
 };
 
 const testLessThanFive = (check) => {
   check.equal(lessThanFive(0), true, 'Zero is always less than five');
-  check.equal(lessThanFive(1), true, 'One is always less than five');
-  check.equal(lessThanFive(7), false, 'Seven is never less than five');
-  check.equal(lessThanFive(2), true, 'Two is always less than five');
+  check.equal(lessThanFive(4), true, 'Four is always less than five');
+  check.equal(lessThanFive(6), false, 'Six is never less than five');
   check.equal(lessThanFive(5), false, 'Five is never less than itself');
 };
 
